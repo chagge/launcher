1dfa29389080edffc0e302fdbee13aabb4c7d15f
dophist <dophist@gmail.com>
2013-08-30 15:22:34 +0800

    python src indentation bug fixed

--- pc/bin/Mac64/HParse.py
+++ pc/bin/Mac64/HParse.py
@@ -7,10 +7,10 @@ SLF = sys.argv[2]
 tags = []
 lines = open(BNF, 'r').readlines()
 for l in lines:
-	fields = l.strip().split()
-	for f in fields:
-		if f[0] == 'T':
-            tags.append(f.strip(';'))
+  fields = l.strip().split()
+  for f in fields:
+    if f[0] == 'T':
+      tags.append(f.strip(';'))
 
 fo = open(SLF, 'w+')
 NumTags = len(tags)
@@ -20,16 +20,16 @@ fo.write('I=0 W=!NULL\n')
 fo.write('I=1 W=!NULL\n')
 fo.write('I=2 W=!NULL\n')
 for i in range(3, NumTags+3):
-	fo.write('I={} W={}\n'.format(i, tags[i-3]))
+  fo.write('I={} W={}\n'.format(i, tags[i-3]))
 
 # links
 l = 0
 for n in range(3, NumTags+3):
-	fo.write('J={} S={} E={}\n'.format(l, 0, n))
-	l += 1
+  fo.write('J={} S={} E={}\n'.format(l, 0, n))
+  l += 1
 for n in range(3, NumTags+3):
-	fo.write('J={} S={} E={}\n'.format(l, n, 1))
-	l += 1
+  fo.write('J={} S={} E={}\n'.format(l, n, 1))
+  l += 1
 
 fo.write('J={} S=1 E=2\n'.format(l))
 
