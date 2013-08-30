91e4e4b4b0ea44aed3713ef8348f33b1c575366f
dophist <dophist@gmail.com>
2013-08-30 13:21:53 +0800

    bug fix on MacOS

--- pc/bin/Mac64/HParse.py
+++ pc/bin/Mac64/HParse.py
@@ -7,28 +7,28 @@ SLF = sys.argv[2]
 tags = []
 lines = open(BNF, 'r').readlines()
 for l in lines:
-	fields = l.strip(';').split()
+	fields = l.strip().split()
 	for f in fields:
 		if f[0] == 'T':
-			tags.append(f)
+            tags.append(f.strip(';'))
 
 fo = open(SLF, 'w+')
 NumTags = len(tags)
 fo.write('N={} L={}\n'.format(NumTags+3, NumTags*2+1))
 # nodes
-fo.write('I=0, W=NULL!\n')
-fo.write('I=1, W=NULL!\n')
-fo.write('I=2, W=NULL!\n')
+fo.write('I=0 W=!NULL\n')
+fo.write('I=1 W=!NULL\n')
+fo.write('I=2 W=!NULL\n')
 for i in range(3, NumTags+3):
-	fo.write('I={}, W={}\n'.format(i, tags[i-3]))
+	fo.write('I={} W={}\n'.format(i, tags[i-3]))
 
 # links
 l = 0
 for n in range(3, NumTags+3):
-	fo.write('J={} S={} E={}\n'.format(l, 0, i))
+	fo.write('J={} S={} E={}\n'.format(l, 0, n))
 	l += 1
 for n in range(3, NumTags+3):
-	fo.write('J={} S={} E={}\n'.format(l, i, 1))
+	fo.write('J={} S={} E={}\n'.format(l, n, 1))
 	l += 1
 
 fo.write('J={} S=1 E=2\n'.format(l))
